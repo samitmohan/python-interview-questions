45c0e4c19b03487492c4b1476bf0a0c74aa0dae1
samitmohan <samitmohan@gmail.com>
2022-10-31 18:23:36 +0530

    tabs

--- angryBirds.py
+++ angryBirds.py
@@ -1,33 +1,34 @@
 import math
+
 def canPlaceBirds(b, n, nests, sep):
-    birds = 1
-    last_location = nests[0]
-    for i in range(1, n - 1):
-        currLoc = nests[i]
-        if (currLoc - last_location >= sep):
-            birds += 1
-            last_location = currLoc
-            if (birds == b):
-                return True
-    return False  # otherwise
+  birds = 1
+  last_location = nests[0]
+  for i in range(1, n - 1):
+      currLoc = nests[i]
+      if (currLoc - last_location >= sep):
+          birds += 1
+          last_location = currLoc
+          if (birds == b):
+              return True
+  return False
 
 def main():
-    nests = [1, 2, 4, 8, 9]
-    b = 3
-    nests.sort()
-    n = len(nests)
+  nests = [1, 2, 4, 8, 9]
+  b = 3
+  nests.sort()
+  n = len(nests)
 
-    # binary search
-    low = 0
-    high = nests[n - 1] - nests[0]
-    ans = -1
-    while low <= high:
-        mid = (low + high)/2
-        canPlace = canPlaceBirds(b, n, nests, mid)
-        if canPlace == True:
-            ans = mid
-            low = mid + 1
-        else:
-            high = mid - 1
-    print(math.ceil(ans))
+  # binary search
+  low = 0
+  high = nests[n - 1] - nests[0]
+  ans = -1
+  while low <= high:
+      mid = (low + high)/2
+      canPlace = canPlaceBirds(b, n, nests, mid)
+      if canPlace == True:
+          ans = mid
+          low = mid + 1
+      else:
+          high = mid - 1
+  print(math.ceil(ans))
 main()
